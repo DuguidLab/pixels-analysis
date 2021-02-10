7f2442220ad93474100f055120f883e5a66e91a7
mcol <mcol@posteo.net>
2021-02-10 16:45:04 +0000

    update thal paper project

--- projects/MC_PhD/thal_paper.py
+++ projects/MC_PhD/thal_paper.py
@@ -1,26 +1,29 @@
 """
-This is a tutorial on how to use the various parts of the pixels pipeline.
+Neuropixels analysis for the Dacre et al motor thalamus paper.
 """
-
 
 import os
 import sys
+
 sys.path.insert(0, os.path.expanduser("~/git/spikesorters"))
 os.environ['KILOSORT3_PATH'] = os.path.expanduser('~/git/Kilosort')
-
+os.environ['KILOSORT2_5_PATH'] = os.path.expanduser('~/git/Kilosort2.5')
 
 import matplotlib.pyplot as plt
-from spikesorters import Kilosort3Sorter
+import seaborn as sns
 
 from pixels import Experiment
 from pixels.behaviours.leverpush import LeverPush, ActionLabels, Events
-from pixtools import spike_times
+from pixtools import clusters, spike_times
+
 
 mice = [
+    #'MCos5',
     #'MCos9',
-    #'C57_724',  # needs doing with KS3
-    'C57_1288723',  # done
-    #'C57_1288727',  # needs doing with KS3
+    #'MCos29',
+    'C57_724',
+    'C57_1288723',
+    'C57_1288727',
 ]
 
 exp = Experiment(
@@ -31,13 +34,17 @@ exp = Experiment(
 )
 
 #exp.sort_spikes()
-exp.process_behaviour()
+#exp.process_behaviour()
 #exp.process_lfp()
 #exp.process_spikes()
 #exp.extract_spikes()
 #exp.process_motion_tracking()
 
-session = 0
-hits = exp.align_trials(ActionLabels.rewarded_push, Events.back_sensor_open, 'spike_times')
-spike_times.across_trials_histogram(hits, session)
+#session = 0
+#hits = exp.align_trials(ActionLabels.rewarded_push, Events.back_sensor_open, 'spike_times')
+#spike_times.across_trials_histogram(hits, session)
+#plt.show()
+
+clusters.depth_profile(exp, curated=False)
+#plt.ylim([-250, 4000])
 plt.show()
