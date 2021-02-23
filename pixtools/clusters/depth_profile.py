a8f70516937696b4664ad640016956725166effe
mcol <mcol@posteo.net>
2021-02-23 19:00:04 +0000

    add group and in_brain kwargs to depth profile function

--- pixtools/clusters/depth_profile.py
+++ pixtools/clusters/depth_profile.py
@@ -8,7 +8,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def depth_profile(exp, session=None, curated=True):
+def depth_profile(exp, session=None, curated=True, group=None, in_brain=False):
     """
     Parameters
     ==========
@@ -23,6 +23,15 @@ def depth_profile(exp, session=None, curated=True):
     curated : bool, optional
         Whether to use curated groupings (good, mua, noise) or uncurated groups from
         kilosort. Default: True.
+
+    group : str, optional
+        Which group to use. One of 'group', 'mua' or 'noise'. Or None (default), which
+        plots them all.
+
+    in_brain : bool, optional
+        Whether to only use units that are in the brain. This required the depth of the
+        probe to be saved into each session's processed/depth.txt files. Default: False.
+
     """
     info = exp.get_cluster_info()
 
@@ -45,7 +54,10 @@ def depth_profile(exp, session=None, curated=True):
     info_flat = []
     for i, ses in enumerate(info):
         for r, rec in enumerate(ses):
-            info_flat.append((exp[i].name, r + 1, rec))
+            if in_brain:
+                info_flat.append((exp[i].name, r + 1, rec, exp[i].get_probe_depth()))
+            else:
+                info_flat.append((exp[i].name, r + 1, rec))
 
     fig, axes = plt.subplots(1, len(info_flat), sharex=True, sharey=True)
     palette = sns.color_palette()
@@ -56,27 +68,45 @@ def depth_profile(exp, session=None, curated=True):
     )
 
     for i, rec in enumerate(info_flat):
-        name, rec_num, data = rec
+        if in_brain:
+            name, rec_num, data, probe_depth = rec
+            data['real_depth'] = probe_depth - data['depth']
+            ylabel = "Depth"
+            y = "real_depth"
+        else:
+            name, rec_num, data = rec
+            ylabel = "Channel position"
+            y = "depth"
+
+        if group:
+            data = data.loc[data['group'] == group]
+
         sns.scatterplot(
             palette=colours,
             x="fr",
-            y="depth",
+            y=y,
             data=data,
             hue=hue,
             ax=axes[i],
+            marker="_",
         )
 
         sns.rugplot(
             palette=colours,
             x="fr",
-            y="depth",
+            y=y,
             data=data,
             hue=hue,
             ax=axes[i],
         )
 
         axes[i].set_xlabel("Firing rate (Hz)")
-        axes[i].set_ylabel("Channel position")
+        axes[i].set_ylabel(ylabel)
         axes[i].set_title(f"{name} rec {rec_num}")
 
+    if in_brain:
+        plt.ylim(reversed(plt.ylim()))
+
     plt.suptitle("Cluster depth profile")
+
+    return fig
