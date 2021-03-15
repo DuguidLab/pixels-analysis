282de6ef2d06562ae7127f74d58ae1775353b2ff
mcol <mcol@posteo.net>
2021-03-15 12:12:30 +0000

    add significant_CI responsiveness test

---
+++ pixtools/responsiveness.py
@@ -0,0 +1,44 @@
+
+import numpy as np
+
+np.random.seed(123)
+
+
+def significant_CI(values, ss=20, CI=95, bs=10000):
+    """
+    Check to see if the bootstrapped confidence intervals of `values` do not overlap
+    with zero.
+
+    Parameters
+    ----------
+    values : list
+        Values from which to extract bootstrap samples.
+
+    ss : int, optional
+        Sample size used when taking sub-samples.
+
+    CI : int/float, optional
+        Confidence interval to compare to 0.
+
+    bs : int, optional
+
+    Returns
+    -------
+    int : 0 if the CI is not different from zero, 1 if it is larger, and -1 if it is
+    smaller.
+
+    """
+    samples = np.array([np.random.choice(values, size=ss) for i in range(bs)])
+    medians = np.median(samples, axis=1)
+    lower = (100 - CI) / 2
+    upper = 100 - lower
+    interval = np.percentile(medians, [lower, upper])
+
+    if interval[0] <= 0 <= interval[1]:
+        # not significant
+        return 0
+
+    if interval[0] > 0:
+        return 1
+    else:
+        return -1
