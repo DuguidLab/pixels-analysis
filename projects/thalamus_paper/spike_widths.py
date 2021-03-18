ef230eaa59601a4cc88200472a2d888cf08217cb
mcol <mcol@posteo.net>
2021-03-18 23:52:22 +0000

    clean up spike widths histograms

--- projects/thalamus_paper/spike_widths.py
+++ projects/thalamus_paper/spike_widths.py
@@ -20,15 +20,31 @@ exp = Experiment(
     '~/duguidlab/CuedBehaviourAnalysis/Data/TrainingJSON',
 )
 
+exp.set_cache(True)
 sns.set(font_scale=0.4)
 fig_dir = Path('~/duguidlab/visuomotor_control/figures').expanduser()
 
 def save(name):
     fig.savefig(fig_dir / name, bbox_inches='tight', dpi=300)
 
-widths = exp.get_spike_widths()
+widths = exp.get_spike_widths(
+    min_depth=500,
+    max_depth=1200,
+)
+
 fig, axes = plt.subplots(3, 1, sharex=True)
-axes[0].hist(widths[0][0].values())
-axes[1].hist(widths[1][0].values())
-axes[2].hist(widths[2][0].values())
-plt.show()
+
+for session in range(len(exp)):
+    values = widths[session][0].values()
+
+    # convert to milliseconds from 30kHz sample points
+    values /= 30
+
+    axes[session].hist(
+        values,
+        bins=range(0, 10),
+    )
+    axes[session].set_title(f"{exp[session].name}")
+
+plt.suptitle('Median spike widths - good deep units')
+save('median_spike_widths_good_deep_units')
