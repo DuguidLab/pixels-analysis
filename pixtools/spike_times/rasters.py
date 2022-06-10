da3f3d8da13bb1024565e92f0ab3e66baf7f8703
mcol <mcol@posteo.net>
2022-06-10 02:25:25 +0100

    big raster improvements

--- pixtools/spike_times/rasters.py
+++ pixtools/spike_times/rasters.py
@@ -6,13 +6,37 @@ Plot a raster of spike times around a specified event.
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import random
+from operator import itemgetter
 from math import ceil
 
 from pixtools.utils import Subplots2D
 
+try:
+    from itertools import pairwise
+except:
+    from itertools import tee
 
-def _raster(per, data, sample, start, subplots, label):
+    def pairwise(iterable):
+        a, b = tee(iterable)
+        next(b, None)
+        return zip(a, b)
+
+
+def _raster(
+    per,
+    data,
+    sample=None,  # Randomly sample this many trials/units
+    start=0,  # Start plotting from this Y axis. Useful for multiple calls.
+    subplots=None,  # Plot onto this Subplots2D, otherwise make some.
+    label=True,  # Write the trial/unit ID in the plot corner.
+    sortby=None,  # Sort trials by this list. A list of tuples that are used to draw patches.
+    more_patches=None,  # Another list of tuples, only used for drawing more patches.
+    size=1,  # Marker size.
+    c=None,  # Marker colour.
+    patch_colors=None,  # Patch colours.
+):
     per_values = data.columns.get_level_values(per).unique()
     per_values.values.sort()
 
@@ -24,43 +48,96 @@ def _raster(per, data, sample, start, subplots, label):
 
     # units if plotting per trial, trials if plotting per unit
     samp_values = list(data.columns.get_level_values(other).unique().values)
+    if sortby:
+        assert len(sortby) == len(samp_values)
+
+    # take sample, ensuring that we are still using the sortby values correctly
     if sample and sample < len(samp_values):
-        sample = random.sample(samp_values, sample)
+        assert not more_patches, "not yet implemented"
+        if sortby:
+            sortby, to_use = zip(
+                *sorted(random.sample(sorted(zip(sortby, samp_values)), sample))
+            )
+            sortby = list(sortby)  # gives us tuples, which we can use for df indexing
+            to_use = list(to_use)
+        else:
+            to_use = sorted(random.sample(samp_values, sample))
+
     else:
-        sample = samp_values
+        to_use = samp_values
+        if sortby:
+            sort_order = np.argsort(list(map(itemgetter(0), sortby)))
+            sortby = np.array(sortby)[sort_order]
+            to_use = np.array(to_use)[sort_order]
+            if more_patches:
+                more_patches = np.array(more_patches)[sort_order]
 
     if subplots is None:
-        subplots = Subplots2D(per_values)
+        subplots = Subplots2D(per_values, sharex=True, sharey=True)
 
-    palette = sns.color_palette()
+    if c is None:
+        palette = sns.color_palette()
+        c = palette[0]
+
+    if not patch_colors:
+        patch_colors = sns.color_palette()
 
     for i, value in enumerate(per_values):
-        val_data = data[value][sample]
-        val_data.columns = np.arange(len(sample)) + start
+        val_data = data[value][to_use]
+        val_data.columns = np.arange(len(to_use)) + start
         val_data = val_data.stack().reset_index(level=1)
 
-        p = sns.scatterplot(
-            data=val_data,
-            x=0,
-            y='level_1',
-            ax=subplots.axes_flat[i],
-            s=0.5,
-            legend=None,
+        ax = subplots.axes_flat[i]
+        ax.scatter(
+            x=val_data[0],
+            y=val_data["level_1"],
+            s=size,
+            linewidths=size * 0.5,
+            facecolors="none",
+            edgecolors=c,
+            alpha=0.9,
         )
-        p.set_yticks([])
-        p.set_xticks([])
-        p.autoscale(enable=True, tight=True)
-        p.get_yaxis().get_label().set_visible(False)
-        p.get_xaxis().get_label().set_visible(False)
-        p.text(
-            0.95, 0.95,
+        ax.set_yticks([])
+        ax.set_xticks([])
+        ax.autoscale(enable=True, tight=True)
+        ax.get_yaxis().get_label().set_visible(False)
+        ax.get_xaxis().get_label().set_visible(False)
+        ax.text(
+            0.95,
+            0.95,
             value,
-            horizontalalignment='right',
-            verticalalignment='top',
+            horizontalalignment="right",
+            verticalalignment="top",
             transform=subplots.axes_flat[i].transAxes,
-            color='0.3',
+            color="0.3",
         )
-        p.axvline(c=palette[2], ls='--', linewidth=0.5)
+        ax.axvline(c="red", ls="--", linewidth=0.5)
+
+        for p, patches in enumerate([sortby, more_patches]):
+            if patches is None:
+                continue
+
+            patch_coords = pd.concat(
+                [
+                    pd.DataFrame(np.arange(len(to_use)) + start).rename(
+                        {0: "y"}, axis=1
+                    ),
+                    pd.DataFrame(patches),
+                ],
+                axis=1,
+            )
+
+            for col, (x1, x2) in enumerate(pairwise(patch_coords.columns[1:])):
+                ax.fill_betweenx(
+                    patch_coords["y"],
+                    patch_coords[x1],
+                    patch_coords[x2],
+                    facecolor=patch_colors[col],
+                    linewidths=0,
+                    alpha=0.6 - p * 0.2,
+                    step="mid",
+                    zorder=-2,
+                )
 
         if not subplots.axes_flat[i].yaxis_inverted():
             subplots.axes_flat[i].invert_yaxis()
@@ -68,17 +145,18 @@ def _raster(per, data, sample, start, subplots, label):
     if label:
         to_label = subplots.to_label
         to_label.get_yaxis().get_label().set_visible(True)
-        to_label.set_ylabel('Trial')
+        to_label.set_ylabel("Trial")
         to_label.get_xaxis().get_label().set_visible(True)
-        to_label.set_xlabel('Time from push (s)')
+        to_label.set_xlabel("Time from push (s)")
 
         # plot legend subplot
         legend = subplots.legend
         legend.text(
-            0, 0.9,
-            'Trial number' if per == 'trial' else 'Unit ID',
+            0,
+            0.9,
+            "Trial number" if per == "trial" else "Unit ID",
             transform=legend.transAxes,
-            color='0.3',
+            color="0.3",
         )
         legend.set_visible(True)
         legend.get_xaxis().set_visible(False)
@@ -87,7 +165,7 @@ def _raster(per, data, sample, start, subplots, label):
     return subplots
 
 
-def per_unit_raster(data, sample=None, start=0, subplots=None, label=True):
+def per_unit_raster(data, **kwargs):
     """
     Plots a spike raster for every unit in the given data with trials as rows.
 
@@ -95,10 +173,10 @@ def per_unit_raster(data, sample=None, start=0, subplots=None, label=True):
     Experiment.align_trials(<action>, <event>, 'spike_times') indexed into to get the
     session and recording.
     """
-    return _raster("unit", data, sample, start, subplots, label)
+    return _raster("unit", data, **kwargs)
 
 
-def per_trial_raster(data, sample=None, start=0, subplots=None, label=True):
+def per_trial_raster(data, **kwargs):
     """
     Plots a spike raster for every trial in the given data with units as rows.
 
@@ -106,51 +184,92 @@ def per_trial_raster(data, sample=None, start=0, subplots=None, label=True):
     Experiment.align_trials(<action>, <event>, 'spike_times') indexed into to get the
     session and recording.
     """
-    return _raster("trial", data, sample, start, subplots, label)
+    return _raster("trial", data, **kwargs)
 
 
-def single_unit_raster(data, ax=None, sample=None, start=0, unit_id=None):
-    # units if plotting per trial, trials if plotting per unit
-    samp_values = list(data.columns.get_level_values('trial').unique().values)
+def single_unit_raster(
+    data,
+    ax=None,
+    sample=None,
+    start=0,
+    unit_id=None,
+    sortby=None,
+    size=1,
+    c=None,
+    patch_colors=None,
+):
+    samp_values = list(data.columns.get_level_values("trial").unique().values)
+    if sortby:
+        assert len(sortby) == len(samp_values)
+
     if sample and sample < len(samp_values):
-        sample = random.sample(samp_values, sample)
+        to_use = sorted(random.sample(samp_values, sample))
+        assert not sortby, "TODO"
     else:
-        sample = samp_values
+        to_use = samp_values
+        if sortby:
+            sortby, to_use = zip(*sorted(zip(sortby, to_use)))
+            sortby = list(sortby)
+            to_use = list(to_use)
 
-    val_data = data[sample]
-    val_data.columns = np.arange(len(sample)) + start
-    val_data = val_data.stack().reset_index(level=1)
+    val_data = data[to_use]
+    val_data.columns = np.arange(len(to_use)) + start
+    val_data = val_data.stack().reset_index(level=1).rename({"level_1": "trial"}, axis=1)
 
     if not ax:
         ax = plt.gca()
 
-    p = sns.scatterplot(
-        data=val_data,
-        x=0,
-        y='level_1',
-        ax=ax,
-        s=1.5,
-        legend=None,
-        edgecolor=None,
+    ax.scatter(
+        x=val_data[0],
+        y=val_data["trial"],
+        s=size,
+        linewidths=size * 0.5,
+        facecolors="none",
+        edgecolors=c,
+        alpha=0.9,
     )
-    p.set_yticks([])
-    p.set_xticks([])
-    p.autoscale(enable=True, tight=True)
-    p.get_yaxis().get_label().set_visible(False)
-    p.get_xaxis().get_label().set_visible(False)
-
-    palette = sns.color_palette()
-    p.axvline(c='black', ls='--', linewidth=0.5)
+    ax.set_yticks([])
+    ax.set_xticks([])
+    ax.autoscale(enable=True, tight=True)
+    ax.get_yaxis().get_label().set_visible(False)
+    ax.get_xaxis().get_label().set_visible(False)
 
     if not ax.yaxis_inverted():
         ax.invert_yaxis()
 
+    if sortby:
+        sortdata = pd.concat(
+            [
+                pd.DataFrame(np.arange(len(to_use)) + start).rename(
+                    {0: "y"}, axis=1
+                ),
+                pd.DataFrame(sortby),
+            ],
+            axis=1,
+        )
+
+        if not patch_colors:
+            patch_colors = sns.color_palette()
+
+        for i, (x1, x2) in enumerate(pairwise(sortdata.columns[1:])):
+            ax.fill_betweenx(
+                sortdata["y"],
+                sortdata[x1],
+                sortdata[x2],
+                facecolor=patch_colors[i],
+                linewidths=0,
+                alpha=0.6,
+                step="mid",
+                zorder=-2,
+            )
+
     if unit_id is not None:
-        p.text(
-            0.95, 0.95,
+        ax.text(
+            0.95,
+            0.95,
             unit_id,
-            horizontalalignment='right',
-            verticalalignment='top',
+            horizontalalignment="right",
+            verticalalignment="top",
             transform=ax.transAxes,
-            color='0.3',
+            color="0.3",
         )
